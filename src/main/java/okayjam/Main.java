ecf12fd80fd219a3211ea33e4f70ea0a3ed2b75a
weiguang <chen2621978@gmail.com>
2017-06-01 20:39:05 +0800

    add new file

--- src/main/java/okayjam/Main.java
+++ src/main/java/okayjam/Main.java
@@ -1,10 +1,13 @@
 package okayjam;
 
+import test.Test;
+
 /**
  * Created by jam on 17-6-1.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Test.testString();
     }
 }
---
+++ src/main/java/util/FileUtil.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by jam on 17-6-1.
+ */
+public class FileUtil {
+}
