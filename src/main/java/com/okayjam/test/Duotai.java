e282f6237afe3f0e953ebcd041e697c22ffa62b6
weiguang <chen2621978@gmail.com>
2017-08-01 15:08:21 +0800

    增加多态测试案例，可以看到继承时构造函数多态的表现

--- src/main/java/com/okayjam/test/Duotai.java
+++ src/main/java/com/okayjam/test/Duotai.java
@@ -3,5 +3,23 @@ package com.okayjam.test;
 /**
  * Created by Weiguang Chen <chen2621978@gmail.com> on 2017/7/31 17:24.
  */
-public class duotai {
+public class Duotai {
+    public static void main(String[] args) {
+        Base base = new Derived();
+        base.echo();
+    }
 }
+
+
+class Base {
+    Base() {echo();}
+     void echo() {
+         System.out.println("Base");}
+};
+
+class Derived extends Base {
+        Derived() {echo();}
+        void echo() {
+            System.out.println("Derived");}
+};
+
