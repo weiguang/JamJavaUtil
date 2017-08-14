479fdc5af51592f6abbb74d615bd7b2f0fe9628f
weiguang <chen2621978@gmail.com>
2017-08-14 17:25:35 +0800

    add 网易有道笔试

---
+++ src/main/test/newcoder/youdao/Main.java
@@ -0,0 +1,91 @@
+package newcoder.youdao;
+
+import java.util.Scanner;
+
+/**
+ * Created by Weiguang Chen <chen2621978@gmail.com> on 2017/8/12 14:37.
+ */
+public class Main {
+    static Scanner sn = new Scanner(System.in);
+    public static void main (String[] args) {
+
+        seq1();
+       // System.out.println(find01String(sn.nextLine()));
+    }
+
+    static int find01String(String src) {
+        int sum = 0;
+        if (src == null || src.length() < 1) {
+            return 0;
+        }
+        int count = 1;
+        char c = src.charAt(0);
+        for (int i = 1; i < src.length(); i++) {
+            if(c == '1' ){
+                if(src.charAt(i) == '0') count++;
+                else count = 1;
+            }else if(c == '0') {
+                if(src.charAt(i) == '1') count++;
+                else {
+                    count = 1;
+                }
+            }
+            c = src.charAt(i);
+            if(count > sum) sum = count;
+        }
+        return sum;
+    }
+
+    static void seq() {
+        int n = sn.nextInt();
+        int[] data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = sn.nextInt();
+            revese(data,0,i);
+        }
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i]);
+            if(i!=data.length -1) System.out.print(" ");
+        }
+    }
+    public static void revese(int[] data, int i, int j){
+        while(i < j) {
+            swap(data,i,j);
+            i++;j--;
+        }
+    }
+    public static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    static void seq1() {
+        int n = sn.nextInt();
+        int[] data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = sn.nextInt();
+        }
+//        int[] d = new int[n];
+//        int index = data.length - 1;
+//        int f = 1;
+//        for (int i = data.length - 1; i >= data.length/2 ; i--) {
+//            d[data.length - i - 1] = data[index--];
+//             if(index >= 0) d[i] = data[index--];
+//        }
+//
+//        for (int i = 0; i < d.length; i++) {
+//            System.out.print(d[i]);
+//            if(i!=d.length -1) System.out.print(" ");
+//        }
+        int k = n;
+        for (int i = n-1; i >= 0 ; i-=2) {
+            System.out.print(data[i]);
+            if(k-- != 1) System.out.print(" ");
+        }
+        for (int i = n%2 ==0 ?0:1; i < n; i+=2) {
+            System.out.print(data[i]);
+            if(k-- != 1) System.out.print(" ");
+        }
+    }
+}
