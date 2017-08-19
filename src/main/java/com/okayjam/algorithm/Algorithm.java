cbe2cc5f9457459cb5960e206f82c862367cb127
weiguang <chen2621978@gmail.com>
2017-08-19 18:23:19 +0800

    add some case

--- src/main/java/com/okayjam/algorithm/Algorithm.java
+++ src/main/java/com/okayjam/algorithm/Algorithm.java
@@ -53,4 +53,33 @@ public class Algorithm {
 //        }
     }
 
+    /**
+     * 菲波那切数列 Fibonacci
+     * 对应有 通项公式求解，递归求解和 非递归求解
+     * @param n
+     * @return
+     */
+    public static long fib(long n) {
+        double sqrt5 = Math.sqrt(5);
+        return (long)(Math.pow(((1 + sqrt5) / 2.0),n) / sqrt5 -
+                Math.pow(((1 - sqrt5) / 2.0),n) / sqrt5);
+    }
+
+//    public static long fib(long n){
+//        return n <= 1 ? n: fib(n - 1) + fib(n - 2);
+//    }
+
+//    public static long fib(long n) {
+//        if(n <= 1) return n;
+//        int[] f = new int[2];
+//        f[0] = 0; f[1] = 1;
+//        int t = 0;
+//        for (int i = 2; i <= n; i++) {
+//            t = f[0] + f[1];
+//            f[0] = f[1];
+//            f[1] = t;
+//        }
+//        return f[1];
+//    }
+
 }
--- src/main/java/com/okayjam/algorithm/MathUtil.java
+++ src/main/java/com/okayjam/algorithm/MathUtil.java
@@ -3,5 +3,14 @@ package com.okayjam.algorithm;
 /**
  * Created by Weiguang Chen <chen2621978@gmail.com> on 2017/8/19 17:10.
  */
-public class Math {
+public class MathUtil {
+    public static boolean isPrime(long n) {
+        if(n < 2) return false;
+        if(n == 2) return true;
+        if(n % 2 == 0) return false;
+        for (int i = 3; i <= java.lang.Math.sqrt(n); i+=2) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
 }
