830ed775c9ca5edc56494f21ea0e5a0e1257bd99
weiguang <chen2621978@gmail.com>
2017-08-20 20:39:06 +0800

    add 58 test class

--- src/main/test/chinahr/jituan58/Main.java
+++ src/main/test/chinahr/jituan58/Main.java
@@ -1,7 +1,18 @@
 package chinahr.jituan58;
 
+import java.util.Scanner;
+
 /**
  * Created by Weiguang Chen <chen2621978@gmail.com> on 2017/8/20 19:26.
  */
 public class Main {
+    Scanner sn = new Scanner(System.in);
+    public static void main (String[] args) {
+        func1();
+    }
+
+    public static void func1() {
+
+    }
+
 }
