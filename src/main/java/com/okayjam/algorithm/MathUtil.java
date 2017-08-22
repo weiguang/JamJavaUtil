cf2e00ce8f7464435cbf8a69bea0dc2857e70d0f
weiguang <chen2621978@gmail.com>
2017-08-22 17:18:57 +0800

    add 注释

--- src/main/java/com/okayjam/algorithm/MathUtil.java
+++ src/main/java/com/okayjam/algorithm/MathUtil.java
@@ -8,7 +8,7 @@ public class MathUtil {
     /**
      * 判断素数 2,3,5,7,11...
      * @param n
-     * @return
+     * @return 如果是素数返回true
      */
     public static boolean isPrime(long n) {
         if(n < 2) return false;
@@ -23,7 +23,7 @@ public class MathUtil {
     /**
      * 计算整数n二进制1的个数
      * @param n
-     * @return
+     * @return 返回1的个数
      */
     public int count1Bits(int n){
         int count = 0;
@@ -38,7 +38,7 @@ public class MathUtil {
      * 给定一个正整数 N，求最小的、比 N 大的正整数 M，使得 M 与 N 的二进制表示中有相同数目的 1
      * 简单枚举
      * @param n
-     * @return
+     * @return 返回下一个和n具有相同1个数的数
      */
     public int GetNextN(int n){
         int k = count1Bits(n);
@@ -52,7 +52,7 @@ public class MathUtil {
      * 给定一个正整数 N，求最小的、比 N 大的正整数 M，使得 M 与 N 的二进制表示中有相同数目的 1
      * O(1)算法实现
      * @param n
-     * @return
+     * @return 返回下一个和n具有相同1个数的数
      */
     public int NextN(int n){
         int x = n&(-n);
@@ -65,7 +65,7 @@ public class MathUtil {
      * 判断一个三位数是否是水仙花数
      * “水仙花数”是指一个三位数，它的各位数字的立方和等于其本身，比如：153=1^3+5^3+3^3
      * @param n
-     * @return
+     * @return 如果是水仙花数返回true，否则返回false
      */
     public static boolean isNarcissisticNumber(int n){
         if(n<100 || n>999) return false;
