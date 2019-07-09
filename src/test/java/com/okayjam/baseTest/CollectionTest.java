fcd68da94a6c8d13b0c9c8f93d6dfe4409e1a3dc
01377846 <weiguangchen@sf-express.com>
2019-07-09 18:07:35 +0800

    增加java8集合steam测试

--- src/test/java/com/okayjam/baseTest/CollectionTest.java
+++ src/test/java/com/okayjam/baseTest/CollectionTest.java
@@ -4,11 +4,77 @@ package com.okayjam.baseTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Weiguang Chen <chen2621978@gmail.com> on 2017/8/8 10:39.
  */
 public class CollectionTest {
+
+
+    @Test
+    public void testStream() {
+        List<Double> list = new ArrayList(1000000);
+        for (int i = 0; i < 10; i++) {
+            list.add(Math.random());
+        }
+        long be;
+        long end;
+        List<Double> list1;
+
+
+
+        be = System.nanoTime();
+
+        list.stream().parallel().forEach(p -> test());
+
+        end = System.nanoTime();
+        System.out.println("Stream: " + (end - be) / 1E6);
+
+
+        be = System.nanoTime();
+        list1 = new ArrayList<>(list.size());
+        for (Double aDouble : list) {
+//            if(aDouble > 0.2) {
+//                list1.add(aDouble);
+//            }
+            test();
+        }
+//        Collections.sort(list1);
+        end = System.nanoTime();
+        System.out.println("normal: " + (end - be) / 1E6);
+
+
+
+
+
+
+/*        be = System.nanoTime();
+        Double max1 = -1d;
+        for (Double aDouble : list) {
+            if (aDouble.compareTo(max1) > 0) {
+                max1 = aDouble;
+            }
+        }
+        end = System.nanoTime();
+        System.out.println("for max:\t " + max1 + "," + (end - be));
+
+        be = System.nanoTime();
+        double max = list.stream().max(Double::compareTo).get();
+        end = System.nanoTime();
+        System.out.println("Stream max:\t " + max + "," + (end - be));*/
+
+//        System.out.println(list1.size());
+//        list1.stream().forEach(System.out::println);
+    }
+
+    public void test() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * 测试集合 Set
      * @author Weiguang Chen <chen2621978@gmail.com> on 2017/8/8 10:39
