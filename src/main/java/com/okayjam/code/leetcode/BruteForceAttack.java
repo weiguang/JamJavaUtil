3d16042bc56189ae2e78173f2cb47acfd82d092c
weiguang <chen2621978@gmail.com>
2022-08-19 14:17:15 +0800

    新增爆力搜索leetcode

--- src/main/java/com/okayjam/code/leetcode/BruteForceAttack.java
+++ src/main/java/com/okayjam/code/leetcode/BruteForceAttack.java
@@ -2,6 +2,7 @@ package com.okayjam.code.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,6 +10,81 @@ import java.util.List;
  * @date 2022/08/16 19:23
  **/
 public class BruteForceAttack {
+	public static void main(String[] args) {
+	    new BruteForceAttack().permute(new int[]{1,2,3});
+	}
+
+	/**
+	 * 77. 组合
+	 * https://leetcode.cn/problems/combinations/
+	 * @param n
+	 * @param k
+	 * @return
+	 */
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> re = new ArrayList<>();
+		int i = 0;
+		while(i+k < n) {
+
+		}
+		return re;
+
+	}
+
+
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> re = new ArrayList<>();
+		List<Integer> t = new ArrayList<>(nums.length + 1);
+		boolean[]vis = new boolean[nums.length];
+		Arrays.sort(nums);
+		backtrackUnique(nums, re, t, vis, 0);
+		return re;
+	}
+
+	public void backtrackUnique(int[] nums, List<List<Integer>> re, List<Integer> t, boolean[]vis, int first) {
+		int n = nums.length;
+		if (first == n) {
+			re.add(new ArrayList<>(t));
+		}
+		for (int i = 0; i < n; i++) {
+			if (vis[i] || (i > 0 && nums[i] == nums[i -1] && vis[i-1])) {
+				continue;
+			}
+			t.add(nums[i]);
+			vis[i] = true;
+			backtrackUnique(nums, re, t, vis, first+1);
+			vis[i] = false;
+			t.remove(first);
+		}
+	}
+
+
+	/**
+	 * 46. 全排列
+	 * https://leetcode.cn/problems/permutations/
+	 * @param nums
+	 * @return
+	 */
+	public  List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> re = new ArrayList<>();
+		List<Integer> t = new ArrayList<>(nums.length + 1);
+		for (int v : nums) {
+			t.add(v);
+		}
+		backtrack(nums.length, re, t, 0);
+		return re;
+	}
+	public void backtrack(int n, List<List<Integer>> re, List<Integer> t, int first) {
+		if (first == n) {
+			re.add(new ArrayList<>(t));
+		}
+		for (int i = first; i < n; i++) {
+			Collections.swap(t, first, i);
+			backtrack(n, re, t, first+1);
+			Collections.swap(t, first, i);
+		}
+	}
+
 	/**
 	 * 78. 子集
 	 * https://leetcode.cn/problems/subsets/
@@ -73,5 +149,5 @@ public class BruteForceAttack {
 		}
 		return re;
 	}
-	
+
 }
