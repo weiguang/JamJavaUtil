11ad978e95ea76fa76a214a74d08aa0a84bcc7aa
jamchen <chen2621978@gmail.com>
2025-09-23 19:46:33 +0800

    add code

--- src/main/java/com/okayjam/code/leetcode/Tree.java
+++ src/main/java/com/okayjam/code/leetcode/Tree.java
@@ -20,6 +20,21 @@ public class Tree {
         }
     }
 
+    /**
+     * 235. 二叉搜索树的最近公共祖先
+     * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+     * @return common
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
 
     /**
      * 230. 二叉搜索树中第 K 小的元素
