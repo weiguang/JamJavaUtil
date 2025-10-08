d6119835983615d5cf4f3847b763e5817ae82ea1
jamchen <chen2621978@gmail.com>
2025-10-08 12:35:53 +0800

    add code 300

--- src/main/java/com/okayjam/code/leetcode/ArrayString.java
+++ src/main/java/com/okayjam/code/leetcode/ArrayString.java
@@ -39,7 +39,8 @@ public class ArrayString {
 //        System.out.println(new ArrayString().maxSlidingWindow(new int[] {1,3,1,2,0,5}, 3));
 //        System.out.println(new ArrayString().numSquares(13));
 //          new ArrayString().moveZeroes(new int[] {0,1,0,3,12});
-        System.out.println(new ArrayString().canWin("+++++"));
+//        System.out.println(new ArrayString().canWin("+++++"));
+        System.out.println(new ArrayString().lengthOfLIS(new int[] {0,8,9,4,2}));
     }
 
     public void swap(int[] nums, int i, int j) {
@@ -55,6 +56,33 @@ public class ArrayString {
      * @return ans
      */
     public int lengthOfLIS(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+
+    public int lengthOfLIS2(int[] nums) {
         if (nums.length ==0) return 0;
         int[] dp = new int[nums.length];
         int ans = 1;
