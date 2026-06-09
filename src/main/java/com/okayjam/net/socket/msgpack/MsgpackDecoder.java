e21c0330e5aebc71a56aa0fee9d04aa07b9e78d6
jam <chen2621978@gmail.com>
2026-06-09 23:38:40 +0800

    更新了maven依赖

--- src/main/java/com/okayjam/net/socket/msgpack/MsgpackDecoder.java
+++ src/main/java/com/okayjam/net/socket/msgpack/MsgpackDecoder.java
@@ -4,28 +4,26 @@ package com.okayjam.net.socket.msgpack;
  * @author Chen weiguang chen2621978@gmail.com
  * @date 2020/10/14 19:40
  **/
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
-import org.msgpack.MessagePack;
-import org.msgpack.template.Templates;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 import java.util.List;
 import java.util.Map;
 
 public class MsgpackDecoder extends MessageToMessageDecoder<ByteBuf> {
+    private static final ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
-        final byte [] array;
+        final byte[] array;
         final int length = msg.readableBytes();
         array = new byte[length];
         msg.getBytes(msg.readerIndex(), array, 0, length);
-        //mp的read方法将其反序列化为object对象
-        System.out.println(new String(array));
-        MessagePack mp =  new MessagePack();
-        mp.register(UserInfo.class);
-        out.add(mp.read(array));
-
+        Map<String, String> result = objectMapper.readValue(array, new TypeReference<Map<String, String>>() {});
+        out.add(result);
     }
 }
--- src/main/java/com/okayjam/net/socket/msgpack/MsgpackEncoder.java
+++ src/main/java/com/okayjam/net/socket/msgpack/MsgpackEncoder.java
@@ -4,16 +4,18 @@ package com.okayjam.net.socket.msgpack;
  * @author Chen weiguang chen2621978@gmail.com
  * @date 2020/10/14 19:40
  **/
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import org.msgpack.MessagePack;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 public class MsgpackEncoder extends MessageToByteEncoder<Object> {
+    private static final ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
+
     @Override
     protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
-        MessagePack mp =  new MessagePack();
-        byte [] raw = mp.write(msg);
+        byte[] raw = objectMapper.writeValueAsBytes(msg);
         out.writeBytes(raw);
     }
 }
