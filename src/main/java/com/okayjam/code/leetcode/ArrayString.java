23c19bf121925797724529717f63d308b6e69e04
jam <chen2621978@gmail.com>
2026-07-27 17:12:01 +0800

    add letcode

--- src/main/java/com/okayjam/code/leetcode/ArrayString.java
+++ src/main/java/com/okayjam/code/leetcode/ArrayString.java
@@ -64,6 +64,20 @@ public class ArrayString {
         nums[j] = temp;
     }
 
+    public int countBattleships(char[][] board) {
+        int ans = 0;
+        for(int i = 0; i < board.length ; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i - 1][j] == 'X') continue;
+                    if (j > 0 && board[i][j - 1] == 'X') continue;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
     public int lastRemaining(int n) {
         int a1 = 1;
         int k = 0, cnt = n, step = 1;
