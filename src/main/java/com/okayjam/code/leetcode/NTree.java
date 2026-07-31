9010cae5c6c1cd497884a7a14132b2b9e32d263a
jam <chen2621978@gmail.com>
2026-07-31 09:10:30 +0800

    leetcode

---
+++ src/main/java/com/okayjam/code/leetcode/NTree.java
@@ -0,0 +1,52 @@
+package com.okayjam.code.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * com.okayjam.code.leetcode
+ *
+ * @author JamChen jamchen@tencent.com
+ * @date 2026/07/31 09:03
+ **/
+public class NTree {
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {return Collections.emptyList();}
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(qSize-- > 0) {
+                Node node = queue.poll();
+                list.add(node.val);
+                if (node.children != null) {
+                    queue.addAll(node.children);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
+
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
--- src/main/java/com/okayjam/code/leetcode/StringJam.java
+++ src/main/java/com/okayjam/code/leetcode/StringJam.java
@@ -42,12 +42,44 @@ public class StringJam {
 //          System.out.println(new StringJam().isAdditiveNumber("011112"));
 //          System.out.println(new StringJam().isAdditiveNumber("12012122436"));
           System.out.println(new StringJam().lengthLongestPath("a.txt"));
+
     }
 
     public void swap(char[] s, int i, int j) {
         char temp = s[i];
         s[i] = s[j];
         s[j] = temp;
+    }
+
+
+    public String originalDigits(String s) {
+        int [] c = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i) ]++;
+        }
+
+        int[] cnt = new int[10];
+        cnt[0] = c['z' ];
+        cnt[2] = c['w'];
+        cnt[4] = c['u'];
+        cnt[6] = c['x'];
+        cnt[8] = c['g'];
+
+        cnt[3] = c['h'] - cnt[8];
+        cnt[5] = c['f'] - cnt[4];
+        cnt[7] = c['s'] - cnt[6];
+
+        cnt[1] = c['o'] - cnt[0] - cnt[2] - cnt[4];
+
+        cnt[9] = c['i'] - cnt[5] - cnt[6] - cnt[8];
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans.append(i);
+            }
+        }
+        return ans.toString();
     }
 
     public boolean isSubsequence(String s, String t) {
