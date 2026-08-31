20f398a8f074f5cee3146cc04de65925c00cb8cc
jam <chen2621978@gmail.com>
2026-08-31 16:31:02 +0800

    leetcode-589+

--- src/main/java/com/okayjam/code/leetcode/NTree.java
+++ src/main/java/com/okayjam/code/leetcode/NTree.java
@@ -1,7 +1,10 @@
 package com.okayjam.code.leetcode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,6 +15,66 @@ import java.util.List;
  * @date 2026/07/31 09:03
  **/
 public class NTree {
+
+    /**
+     * 590. N-ary Tree Postorder Traversal
+     * @param root root
+     * @return ans
+     */
+    public List<Integer> postorder(Node root) {
+        if (root == null) return postorderAns;
+        for (Node child : root.children) {
+            postorder(child);
+        }
+        postorderAns.add(root.val);
+        return postorderAns;
+    }
+
+    List<Integer> postorderAns = new ArrayList<>();
+    public List<Integer> postorder2(Node root) {
+        if (root == null) return postorderAns;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node pop = stack.pop();
+            for (int i = 0; i < pop.children.size() ; i++) {
+                stack.push(pop.children.get(i));
+            }
+            postorderAns.add(pop.val);
+        }
+        Collections.reverse(postorderAns);
+        return postorderAns;
+    }
+
+    /**
+     * 589. N-ary Tree Preorder Traversal
+     * 递归和迭代
+     * @param root root
+     * @return ans
+     */
+    public List<Integer> preorder(Node root) {
+        if (root == null) return preorderAns;
+        preorderAns.add(root.val);
+        for (Node child : root.children) {
+            preorder(child);
+        }
+        return preorderAns;
+    }
+
+    List<Integer> preorderAns = new ArrayList<>();
+    public List<Integer> preorder2(Node root) {
+        if (root == null) return preorderAns;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node pop = stack.pop();
+            preorderAns.add(pop.val);
+            for (int i = pop.children.size() - 1; i >= 0 ; i--) {
+                stack.push(pop.children.get(i));
+            }
+        }
+        return preorderAns;
+    }
 
     /**
      * 559. N 叉树的最大深度
