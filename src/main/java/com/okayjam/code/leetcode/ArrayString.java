2ffd2d8e62bc4e7bb73bd795695d069552735402
jam <chen2621978@gmail.com>
2026-09-20 22:48:51 +0800

    leetcode-640

--- src/main/java/com/okayjam/code/leetcode/ArrayString.java
+++ src/main/java/com/okayjam/code/leetcode/ArrayString.java
@@ -80,6 +80,49 @@ public class ArrayString {
     }
 
     /**
+     * 640. 求解方程
+     * @param equation equation
+     * @return ans
+     */
+    public String solveEquation(String equation) {
+        int factor = 0, val = 0;
+        int index = 0, n = equation.length(), sign1 = 1; // 等式左边默认系数为正
+        while (index < n) {
+            if (equation.charAt(index) == '=') {
+                sign1 = -1; // 等式右边默认系数为负
+                index++;
+                continue;
+            }
+
+            int sign2 = sign1, number = 0;
+            // 记录 number 是否有效
+            boolean valid = false;
+            // 去掉前面的符号
+            if (equation.charAt(index) == '-' || equation.charAt(index) == '+') {
+                sign2 = (equation.charAt(index) == '-') ? -sign1 : sign1;
+                index++;
+            }
+            while (index < n && Character.isDigit(equation.charAt(index))) {
+                number = number * 10 + (equation.charAt(index) - '0');
+                index++;
+                valid = true;
+            }
+
+            if (index < n && equation.charAt(index) == 'x') { // 变量
+                factor += valid ? sign2 * number : sign2;
+                index++;
+            } else { // 数值
+                val += sign2 * number;
+            }
+        }
+
+        if (factor == 0) {
+            return val == 0 ? "Infinite solutions" : "No solution";
+        }
+        return "x=" + (-val / factor);
+    }
+
+    /**
      * 638. 大礼包
      * @param price price
      * @param special special
