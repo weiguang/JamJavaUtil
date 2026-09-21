27a5940867f0cacbce0f3731d0cdb6306a82b178
jam <chen2621978@gmail.com>
2026-09-21 23:21:52 +0800

    Update ArrayString.java

--- src/main/java/com/okayjam/code/leetcode/ArrayString.java
+++ src/main/java/com/okayjam/code/leetcode/ArrayString.java
@@ -2596,19 +2596,13 @@ public class ArrayString {
     public int[] findErrorNums(int[] nums) {
         int[] ans = new int[2];
         for (int i = 0; i < nums.length; ) {
-            if (nums[i] != i + 1) {
-                if (nums[i] == nums[nums[i] - 1]) {
-                    ans[0] = nums[i];
-                    i++;
-                } else {
-                    swap(nums, i, nums[i] - 1);
-                }
-            } else {
-                i++;
-            }
+            if (nums[i] != i + 1 &&  nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else i++;
         }
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1) {
+                ans[0] = nums[i];
                 ans[1] = i + 1;
                 break;
             }
